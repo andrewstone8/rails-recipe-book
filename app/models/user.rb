752c5708d46d26b64e34d606c7dd3312312e54f2
andrewstone8 <andrewstone8@gmail.com>
2016-08-30 21:50:45 -0700

    minor change to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :recipes
+  has_secure_password
 
-  
 end
